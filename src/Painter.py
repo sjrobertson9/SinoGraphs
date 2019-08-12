7e65db52368345a4fe49417b280c36cd3865196a
Sarah Robertson <sarah.robertson@aggiemail.usu.edu>
2019-08-12 16:27:09 +0200

    added multipliers to Painter and Patterns

--- src/Painter.py
+++ src/Painter.py
@@ -1,18 +1,19 @@
 from Function import getPoints
+from Patterns import mults
 import turtle
-
-# TODO: add multipliers to each list ([0, 1])
 
 def paint(patterns, pattern="city"):
     functions = getPoints(patterns[pattern])
+    xmult = 3.1
+    ymult = mults[pattern]
     # print(functions)
     turtle.showturtle()
-    turtle.speed(10)
+    turtle.speed(0)
     for func in functions:
         turtle.pu()
         x = -125
         for i in range(50):
-            turtle.goto(x*3, functions[str(func)][i]*2)
+            turtle.goto(x*xmult, functions[str(func)][i]*ymult)
             x += 5
             turtle.pd()
         turtle.pu()
--- src/Patterns.py
+++ src/Patterns.py
@@ -1,6 +1,6 @@
 # Contains a whole lot of lists I guess
 patterns = {
-    "test": ["sin(x)", "-sin(x)", "-4sin(x)", "4sin(x)", "8sin(x)",
+    "test": ["3.1", "6", "sin(x)", "-sin(x)", "-4sin(x)", "4sin(x)", "8sin(x)",
              "-8sin(x)", "cos(x)", "-cos(x)", "50sin(x)", "-50sin(x)"],
     "wiggle": ["4sin(1/2x)", "-2sin(1/2x)", "6sin(1/2x)", "-6sin(1/2x)",
                "-2sin(1/2x)", "2sin(1/2x)", "2cos(1/2x)", "-2cos(1/2x)"],
@@ -9,7 +9,27 @@ patterns = {
     "fingers": ["tan(1/3x)", "tan(-1/3x)", "tan(1/4x)", "tan(-1/4x)",
                 "3tan(1/3x)", "-3tan(1/3x)", "3tan(1/4x)", "-3tan(1/4x)",
                 "3tan(1/4x)", "-3tan(1/4x)", "2sin(1/2x)", "-2sin(1/2x)"],
-    "city": ["15sin(2x)", "-15sin(2x", "4cos(x)", "-4cos(x)",
-             "10sin(1/2x)", "-10sin(1/2x)", "tan(x)"],
-    "chunk": ["sin(x)", "cos(x)"]
+    "city": ["15sin(2x)", "-15sin(2x)", "4cos(x)", "-4cos(x)",
+             "10sin(1/2x)", "-10sin(1/2x)", "tan(x)", "-tan(x)"],
+    "chunk": ["sin(1/2x)", "cos(1/2x)", "5sin(1/2x)", "5cos(1/2x)",
+              "10sin(1/2x)", "10cos(1/2x)", "15sin(1/2x)", "15cos(1/2x)",
+              "20sin(1/2x)", "20cos(1/2x)", "25sin(1/2x)", "25cos(1/2x)",
+              "30sin(1/2x)", "30cos(1/2x)", "35sin(1/2x)", "35cos(1/2x)",
+              "40sin(1/2x)", "40cos(1/2x)", "45sin(1/2x)", "45cos(1/2x)",
+              "50sin(1/2x)", "50cos(1/2x)", "55sin(1/2x)", "55cos(1/2x)",
+              "60sin(1/2x)", "60cos(1/2x)", "65sin(1/2x)", "65cos(1/2x)",
+              "70sin(1/2x)", "70cos(1/2x)", "75sin(1/2x)", "75cos(1/2x)",
+              "80sin(1/2x)", "80cos(1/2x)", "85sin(1/2x)", "85cos(1/2x)",
+              "90sin(1/2x)", "90cos(1/2x)", "95sin(1/2x)", "95cos(1/2x)",
+              "100sin(1/2x)", "100cos(1/2x)"]
+}
+# Contains the x y multipliers for each list because I'm lazy
+
+mults = {
+    "test": 6,
+    "wiggle": 35,
+    "eyes": 20,
+    "fingers": 90,
+    "city": 17,
+    "chunk": 3
 }
