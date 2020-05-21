c1a7c72978e18065b0ce675e675bea3f6557551c
Sarah Robertson <sarah.robertson@aggiemail.usu.edu>
2020-05-20 22:37:55 -0600

    got color attribute working

--- src/Painter.py
+++ src/Painter.py
@@ -12,8 +12,7 @@ def paint(patterns, pattern="test", color="blue"):
     turtle.showturtle()
     turtle.speed(0)
     for func in functions:
-        # turtle.color(getColor(color))
-        turtle.color("black")
+        turtle.color(getColor(color))
         turtle.pu()
         x = -125
         for i in range(50):
@@ -25,6 +24,8 @@ def paint(patterns, pattern="test", color="blue"):
 
 
 def getColor(color):
+    #TODO: make more palettes
+    #TODO: figure out how to make a gradient -- global variable?? reset in main??
     red = ["maroon", "darkorange", "mistyrose", "chocolate", "tomato",
            "coral", "darksalmon", "sienna"]
     blue = ["skyblue", "slategrey", "teal", "paleturquoise", "royalblue",
